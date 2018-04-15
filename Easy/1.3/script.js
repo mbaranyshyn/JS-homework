0a8911c6aaa6975c3fff50124ea4db94aeda5577
mbaranyshyn <mbaranyshyn@gmail.com>
2018-04-15 23:50:56 +0300

    add Math.round

--- Easy/1.3/script.js
+++ Easy/1.3/script.js
@@ -3,6 +3,6 @@ function averageNumber(arr) {
     for (var i = 0, j = arr.length; i < j; i++) {
         sum += arr[i];
     }
-    return sum / arr.length;
+    return Math.round(sum / arr.length);
 }
 console.log(averageNumber([1, 4, 2]));
